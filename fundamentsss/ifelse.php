a1956a7e628326f1e08635eaba766d9fb54c7f20
Angel Jesse Dampil <108660194+Mochimochi07@users.noreply.github.com>
2023-02-27 14:49:55 +0800

    Create ifelse.php

---
+++ fundamentsss/ifelse.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+
+$age = 1;
+if ($age >= 18) {
+    echo "You are an adult.";
+} else {
+    echo "You are not yet an adult.";
+}
+
+?>
+
+</body>
+</html>
